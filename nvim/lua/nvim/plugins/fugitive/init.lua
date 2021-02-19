017e5a93f2529888a8b90e07b44485f7427afab6
Metehan Gulac <metehanglc@protonmail.com>
2021-02-20 00:50:28 +0300

    new lua confs added

--- nvim/lua/nvim/plugins/fugitive/init.lua
+++ nvim/lua/nvim/plugins/fugitive/init.lua
@@ -1,2 +1,3 @@
 require('nvim.plugins.fugitive.keybinds')
 require('nvim.plugins.fugitive.variables')
+require('nvim.plugins.fugitive.options')
--- nvim/lua/nvim/utils/option.lua
+++ nvim/lua/nvim/utils/option.lua
@@ -1,34 +1,46 @@
 local Type = {
-  GLOBAL_OPTION = 'o',
-  WINDOW_OPTION = 'wo',
-  BUFFER_OPTION = 'bo',
+	GLOBAL_OPTION = 'o',
+	WINDOW_OPTION = 'wo',
+	BUFFER_OPTION = 'bo'
 }
 
-local add_options = function (option_type, options)
-  if type(options) ~= 'table' then
-    error('options should be a type of "table"')
-    return
-  end
+local add_options = function (option_type, id, options)
+	if type(id) == 'table' then
+		options = id
+		id = 0
+	end
 
-  local vim_option = vim[option_type]
+	if type(options) ~= 'table' then
+		error('options should be a type of "table"')
+		return
+	end
 
-  for key, value in pairs(options) do
-    vim_option[key] = value
-  end
+	for key, value in pairs(options) do
+		-- adding options to vim
+		-- id condition is there to make it compatible with global options
+		-- global options has no id
+		-- vim['o']['mouse'] = 4
+		-- vim['wo'][10]['number'] = true
+		if id == 0 then
+			vim[option_type][key] = value
+		else
+			vim[option_type][id][key] = value
+		end
+	end
 end
 
 local Option = {}
 
-Option.g = function (options)
-  add_options(Type.GLOBAL_OPTION, options)
+Option.g = function(options)
+	add_options(Type.GLOBAL_OPTION, options)
 end
 
-Option.w = function (options)
-  add_options(Type.WINDOW_OPTION, options)
+Option.w = function(id, options)
+	add_options(Type.WINDOW_OPTION, id, options)
 end
 
-Option.b = function (options)
-  add_options(Type.BUFFER_OPTION, options)
+Option.b = function(id, options)
+	add_options(Type.BUFFER_OPTION, id, options)
 end
 
 return Option
