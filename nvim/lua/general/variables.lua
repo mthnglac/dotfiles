f0cb69303bee0a16880dbd9e25955df0e712c7df
Metehan Gulac <metehanglc@protonmail.com>
2021-03-13 23:41:49 +0300

    UPDATE: some changes

--- nvim/lua/general/variables.lua
+++ nvim/lua/general/variables.lua
@@ -4,13 +4,13 @@ Variable.g({
 })
 
 Variable.g({
-    -- neoformat ------------------------------------------------------------------
+    -- neoformat
 	neoformat_enabled_python = {'black'},
-    -- Enable alignment ------------------------------------------------------------------
+    -- Enable alignment
 	neoformat_basic_format_align = 1,
-    -- Enable tab to spaces conversion ------------------------------------------------------------------
+    -- Enable tab to spaces conversion
 	neoformat_basic_format_retab = 1,
-    -- Enable trimmming of trailing whitespace ------------------------------------------------------------------
+    -- Enable trimmming of trailing whitespace
 	neoformat_basic_format_trim = 1,
 })
 
