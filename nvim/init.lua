2374154fcf918a09fb9885b790e1b372be015743
Metehan Gulac <metehanglc@protonmail.com>
2021-03-16 22:02:00 +0300

    UPDATE: reload fix

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,10 +2,13 @@
 require('utils')
 
 -- vim core settings
+Reload.reload_module('general')
 require('general')
 
 -- vim-plug settings & plugins
+Reload.reload_module('install-plugins')
 require('install-plugins')
 
 -- vim plugins settings
+Reload.reload_module('plugins')
 require('plugins')
--- nvim/lua/utils/init.lua
+++ nvim/lua/utils/init.lua
@@ -4,6 +4,7 @@ Keybind = require('utils.keybind')
 Command = require('utils.command')
 Augroup = require('utils.augroup')
 Editor = require('utils.editor')
+Reload = require('utils.reload')
 
 Vim = {
   Option = Option,
@@ -12,4 +13,5 @@ Vim = {
   Command = Command,
   Augroup = Augroup,
   Editor = Editor,
+  Reload = Reload,
 }
