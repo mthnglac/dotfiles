62f1761515092c9f024baf7dba4d415150441171
Metehan Gulac <metehanglc@protonmail.com>
2021-04-05 23:27:04 +0300

    FIX: lua syntax typos.

--- nvim/lua/install-plugins.lua
+++ nvim/lua/install-plugins.lua
@@ -1,8 +1,8 @@
 local vim = vim
 local function plug(path, config)
   vim.validate {
-    path = {path, 's'};
-    config = {config, vim.tbl_islist, 'an array of packages'};
+    path = {path, 's'},
+    config = {config, vim.tbl_islist, 'an array of packages'},
   }
   vim.fn["plug#begin"](path)
   for _, v in ipairs(config) do
@@ -21,110 +21,111 @@ end
 
 plug(tostring(os.getenv("HOME")) .. '/.vim/plugged', {
   -- Tetris game
-  'alec-gibson/nvim-tetris';
+  'alec-gibson/nvim-tetris',
   -- Treesitter
-  'nvim-treesitter/nvim-treesitter';
+  'nvim-treesitter/nvim-treesitter',
   -- Telescope
-  'nvim-telescope/telescope.nvim';
-  'nvim-lua/plenary.nvim';
-  'nvim-lua/popup.nvim';
-  'nvim-telescope/telescope-fzy-native.nvim';
-  'nvim-telescope/telescope-symbols.nvim';
+  'nvim-telescope/telescope.nvim',
+  'nvim-lua/plenary.nvim',
+  'nvim-lua/popup.nvim',
+  'nvim-telescope/telescope-fzy-native.nvim',
+  'nvim-telescope/telescope-symbols.nvim',
+  'kyazdani42/nvim-web-devicons',
   -- emberjs
-  'joukevandermaas/vim-ember-hbs';
+  'joukevandermaas/vim-ember-hbs',
   -- ember-test-runner
-  'toranb/vim-ember-cli-test-runner';
+  'toranb/vim-ember-cli-test-runner',
   -- dispatcher
-  'tpope/vim-dispatch';
+  'tpope/vim-dispatch',
   -- Git - blamer
-  'ttys3/nvim-blamer.lua';
+  'ttys3/nvim-blamer.lua',
   -- Git - show commit of current line
-  'rhysd/git-messenger.vim';
+  'rhysd/git-messenger.vim',
   -- Git plugin for vim. Bestest of bests!!!
-  'tpope/vim-fugitive';
+  'tpope/vim-fugitive',
   -- cheat.sh with popup plugin
-  'RishabhRD/popfix';
-  'RishabhRD/nvim-cheat.sh';
+  'RishabhRD/popfix',
+  'RishabhRD/nvim-cheat.sh',
   -- debugger
-  'puremourning/vimspector';
+  'puremourning/vimspector',
   -- Godot vim plugin
-  'habamax/vim-godot';
+  'habamax/vim-godot',
   -- check vim startup time
-  'tweekmonster/startuptime.vim';
+  'tweekmonster/startuptime.vim',
   -- CoC
-  {'neoclide/coc.nvim'; branch = 'release'};
+  {'neoclide/coc.nvim', ['branch'] = 'release'},
   -- Snippets
-  'mthnglac/vim-snippets';
+  'mthnglac/vim-snippets',
   -- A Vim plugin that runs 'gofmt' when you save
-  'tweekmonster/gofmt.vim';
+  'tweekmonster/gofmt.vim',
   -- displaying vertical lines
-  'Yggdroot/indentLine';
+  'Yggdroot/indentLine',
   -- surroundings": parentheses, brackets, quotes vs.
-  'tpope/vim-surround';
+  'tpope/vim-surround',
   -- manage virtualenv
-  'jmcantrell/vim-virtualenv';
+  'jmcantrell/vim-virtualenv',
   -- file browser
-  'preservim/nerdtree';
-  'Xuyuanp/nerdtree-git-plugin';
+  'preservim/nerdtree',
+  'Xuyuanp/nerdtree-git-plugin',
   -- .gitignore plugin
-  'gisphm/vim-gitignore';
+  'gisphm/vim-gitignore',
   -- for showing git sign: inserted, modified, deleted vs..
-  'airblade/vim-gitgutter';
+  'airblade/vim-gitgutter',
   -- commenter
-  'preservim/nerdcommenter';
+  'preservim/nerdcommenter',
   -- Insert or delete brackets, parens, quotes in pair.
-  'jiangmiao/auto-pairs';
+  'jiangmiao/auto-pairs',
   -- for viewing man pages in vim.
-  'vim-utils/vim-man';
+  'vim-utils/vim-man',
   -- crab looking tool. looking for what you do shortly before
-  'mbbill/undotree';
+  'mbbill/undotree',
   -- A collection of language packs.
-  'sheerun/vim-polyglot';
+  'sheerun/vim-polyglot',
   -- Vlang
-  'ollykel/v-vim';
+  'ollykel/v-vim',
   -- Simple TODO plugin
-  'vuciv/vim-bujo';
+  'vuciv/vim-bujo',
   -- compiler plugin
-  'tpope/vim-dispatch';
+  'tpope/vim-dispatch',
   -- icons pack
-  'ryanoasis/vim-devicons';
+  'ryanoasis/vim-devicons',
   -- Prettier plugin
-  {'prettier/vim-prettier'; run = 'yarn install'};
+  {'prettier/vim-prettier', ['do'] = 'yarn install'},
   -- Kotlin plugin
-  'udalov/kotlin-vim';
+  'udalov/kotlin-vim',
   -- Dart plugin
-  'dart-lang/dart-vim-plugin';
+  'dart-lang/dart-vim-plugin',
   -- Presentation
-  'junegunn/goyo.vim';
-  'junegunn/limelight.vim';
-  'vimwiki/vimwiki';
+  'junegunn/goyo.vim',
+  'junegunn/limelight.vim',
+  'vimwiki/vimwiki',
   -- firenvim
-  {'glacambre/firenvim'; run = function() vim.fn['firenvim#install'](69) end};
+  {'glacambre/firenvim', ['do'] = function(_) vim.fn['firenvim#install'](69) end},
   -- An always-on highlight for a unique character in every word on a line to
   -- help you use f, F and family.
-  'unblevable/quick-scope';
+  'unblevable/quick-scope',
   -- repeat key for '.' key
-  'tpope/vim-repeat';
+  'tpope/vim-repeat',
   -- better whitespace
-  'ntpeters/vim-better-whitespace';
+  'ntpeters/vim-better-whitespace',
   -- buffer closer
-  'Asheq/close-buffers.vim';
+  'Asheq/close-buffers.vim',
   -- matchup with closing match
-  'andymass/vim-matchup';
+  'andymass/vim-matchup',
   -- popup terminal
-  'voldikss/vim-floaterm';
+  'voldikss/vim-floaterm',
   -- colorschemes
-  'colepeters/spacemacs-theme.vim';
-  'gruvbox-community/gruvbox';
-  'sainnhe/gruvbox-material';
-  'phanviet/vim-monokai-pro';
-  'vim-airline/vim-airline';
-  'vim-airline/vim-airline-themes';
-  'flazz/vim-colorschemes';
-  'chriskempson/base16-vim';
-  {'dracula/vim'; as = 'dracula'};
+  'colepeters/spacemacs-theme.vim',
+  'gruvbox-community/gruvbox',
+  'sainnhe/gruvbox-material',
+  'phanviet/vim-monokai-pro',
+  'vim-airline/vim-airline',
+  'vim-airline/vim-airline-themes',
+  'flazz/vim-colorschemes',
+  'chriskempson/base16-vim',
+  {'dracula/vim', ['as'] = 'dracula'},
   -- a vim game . thanks to ThePrimeagen!
-  {'ThePrimeagen/vim-be-good'; run = '.\\install.sh'};
+  'ThePrimeagen/vim-be-good',
   -- vim command tracking tool.
-  'ThePrimeagen/vim-apm';
+  'ThePrimeagen/vim-apm',
 })
--- nvim/lua/plugins/firenvim/commands.lua
+++ nvim/lua/plugins/firenvim/commands.lua
@@ -1,7 +1,7 @@
 Augroup.cmds({
     firenvim = {
-        {"BufEnter", "github.com_*.txt set filetype=markdown"};
-        {"BufEnter", "txti.es_*.txt set filetype=typescript"};
-        {"BufEnter", "stackoverflow_*.txt set filetype=markdown"};
-    };
+        {"BufEnter", "github.com_*.txt set filetype=markdown"},
+        {"BufEnter", "txti.es_*.txt set filetype=typescript"},
+        {"BufEnter", "stackoverflow_*.txt set filetype=markdown"},
+    },
 })
