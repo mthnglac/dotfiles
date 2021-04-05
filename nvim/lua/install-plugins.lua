352f365ff072f71382fcb0d84b842d7ffd6ee04b
Metehan Gulac <metehanglc@protonmail.com>
2021-04-05 23:47:32 +0300

    UPDATE: treesitter conf.

--- nvim/lua/install-plugins.lua
+++ nvim/lua/install-plugins.lua
@@ -23,7 +23,7 @@ plug(tostring(os.getenv("HOME")) .. '/.vim/plugged', {
   -- Tetris game
   'alec-gibson/nvim-tetris',
   -- Treesitter
-  'nvim-treesitter/nvim-treesitter',
+  {'nvim-treesitter/nvim-treesitter', ['do'] = '<Cmd>TSUpdate'},
   -- Telescope
   'nvim-telescope/telescope.nvim',
   'nvim-lua/plenary.nvim',
