db8125b10bbbd4f3ad7bfee3879f819e9bb6c861
Metehan Gulac <metehanglc@protonmail.com>
2021-04-20 18:37:54 +0300

    UPDATE: telescope excepting big files now.

--- nvim/lua/plugins/telescope/settings.lua
+++ nvim/lua/plugins/telescope/settings.lua
@@ -1,4 +1,19 @@
 local actions = require('telescope.actions')
+local previewers = require('telescope.previewers')
+
+local new_maker = function(filepath, bufnr, opts)
+  opts = opts or {}
+
+  filepath = vim.fn.expand(filepath)
+  vim.loop.fs_stat(filepath, function(_, stat)
+    if not stat then return end
+    if stat.size > 100000 then
+      return
+    else
+      previewers.buffer_previewer_maker(filepath, bufnr, opts)
+    end
+  end)
+end
 
 require('telescope').setup({
   defaults = {
@@ -52,15 +67,15 @@ require('telescope').setup({
     qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
 
     -- Developer configurations: Not meant for general override
-    buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
+    --buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
+    buffer_previewer_maker = new_maker,
+    extensions = {
+      fzy_native = {
+        override_generic_sorter = false,
+        override_file_sorter = true,
+      }
+    },
   },
-  extensions = {
-    fzy_native = {
-      override_generic_sorter = false,
-      override_file_sorter = true,
-      case_mode = "smart_case",
-    }
-  }
 })
 
 require('telescope').load_extension('fzy_native')
