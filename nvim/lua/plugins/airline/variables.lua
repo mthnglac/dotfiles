e6cadce68a373b004e3b8251d1bf58b6c85400d4
Metehan Gulac <metehanglc@protonmail.com>
2021-06-11 10:29:23 +0300

    FIX: airline slowing solved with caching.

--- nvim/lua/plugins/airline/variables.lua
+++ nvim/lua/plugins/airline/variables.lua
@@ -1,6 +1,7 @@
 Variable.g({
 	airline_theme = "luna",
-	airline_powerline_fonts = 1
+	airline_powerline_fonts = 1,
+	airline_highlighting_cache = 1,
 })
 
 vim.g["airline#extensions#tabline#enabled"] = 1
