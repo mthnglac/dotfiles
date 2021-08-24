d310954ef212da151ab71da683080150926396f7
Metehan Gulac <metehanglc@protonmail.com>
2021-08-24 18:10:05 +0300

    UPDATE: new gitsigns.nvim settings

--- nvim/lua/plugins/git-signs-nvim/settings.lua
+++ nvim/lua/plugins/git-signs-nvim/settings.lua
@@ -6,21 +6,39 @@ require('gitsigns').setup({
     topdelete = { hl = 'GitSignsDelete', text = '‾', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
     changedelete = { hl = 'GitSignsChange', text = '~', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
   },
+  signcolumn = true,
   numhl = false,
   linehl = false,
+  word_diff = false,
   keymaps = {},
   watch_index = {
     interval = 1000,
     follow_files = true,
   },
+  attach_to_untracked = true,
+  current_line_blame = false,
   current_line_blame_opts = {
     virt_text = true,
     virt_text_pos = 'eol',
     delay = 0,
   },
+  current_line_blame_formatter_opts = {
+    relative_time = false,
+  },
   sign_priority = 6,
   update_debounce = 100,
   status_formatter = nil, -- Use default
-  word_diff = false,
-  use_internal_diff = true,  -- If luajit is present
+  max_file_length = 40000,
+  preview_config = {
+    -- Options passed to nvim_open_win
+    border = 'single',
+    style = 'minimal',
+    relative = 'cursor',
+    row = 0,
+    col = 1
+  },
+  use_internal_diff = true,  -- If vim.diff or luajit is present
+  yadm = {
+    enable = false
+  },
 })
