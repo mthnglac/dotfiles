a97458374392f5eb71f526e3f4ad94e68db9ca68
Metehan Gulac <metehanglc@protonmail.com>
2022-03-15 18:04:32 +0300

    autocmd clearized

--- nvim/lua/general/commands.lua
+++ nvim/lua/general/commands.lua
@@ -1,22 +1,7 @@
-Command.cmd({
-    'syntax on',
-    'filetype on',
-    'filetype plugin on',
-    'filetype indent on',
-    -- tab confs
-    'autocmd FileType python setlocal sw=4',
-    'autocmd FileType python setlocal ts=4',
-    'autocmd FileType python setlocal sts=4',
-})
-
---Augroup.cmds({
-    --MTHNGLAC = {
-        ----{"BufWritePre", "* :call TrimWhitespace()"},
-    --},
---})
-
 -- highlight on yank!!!
 vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = "*",
-  callback = function() require("vim.highlight").on_yank({ timeout = 400 }) end
+  callback = function()
+    require("vim.highlight").on_yank({ timeout = 400 })
+  end
 })
