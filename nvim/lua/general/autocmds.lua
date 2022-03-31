aa842301f6490569afeb39ea845da7aa61f87b3d
Metehan Gulac <metehanglc@protonmail.com>
2022-03-31 18:45:40 +0300

    cursorline&cursorcolumn highlight

--- nvim/lua/general/autocmds.lua
+++ nvim/lua/general/autocmds.lua
@@ -5,3 +5,25 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     require("vim.highlight").on_yank({ timeout = 400 })
   end
 })
+
+local groupCursorEffect = vim.api.nvim_create_augroup("HighlighMyCursor", { clear = true })
+vim.api.nvim_create_autocmd({'WinEnter,BufWinEnter,VimEnter'},
+  {
+    pattern = "*",
+    callback = function()
+      vim.wo.cursorline = true
+      vim.wo.cursorcolumn = true
+    end,
+    group = groupCursorEffect
+  }
+)
+vim.api.nvim_create_autocmd('WinLeave',
+  {
+    pattern = "*",
+    callback = function()
+      vim.wo.cursorline = false
+      vim.wo.cursorcolumn = false
+    end,
+    group = groupCursorEffect
+  }
+)
