c501e25572cefdc35a772ae2e6a6033bf66e7b53
Metehan Gulac <metehanglc@protonmail.com>
2022-05-30 09:27:59 +0300

    nvim-tree deprecated confs & news added.

--- nvim/lua/plugins/nvim-tree/init.lua
+++ nvim/lua/plugins/nvim-tree/init.lua
@@ -1,3 +1,2 @@
-require('plugins.nvim-tree.variables')
 require('plugins.nvim-tree.keybinds')
 require('plugins.nvim-tree.settings')
--- nvim/lua/plugins/nvim-tree/settings.lua
+++ nvim/lua/plugins/nvim-tree/settings.lua
@@ -40,6 +40,16 @@ require('nvim-tree').setup({
   },
   --false by default, this option shows indent markers when folders are open
   renderer = {
+    --0 by default, append a trailing slash to folder names
+    add_trailing = true,
+    -- 0 by default, compact folders that only contain a single folder into one node in the file tree
+    group_empty = true,
+    --0 by default, will enable file highlight for git attributes (can be used without the icons).
+    highlight_git = true,
+    --0 by default, will enable folder and file icon highlight for opened files/directories.
+    highlight_opened_files = "none",
+    --This is the default. See :help filename-modifiers for more options
+    root_folder_modifier = ":~",
     indent_markers = {
       enable = true,
       icons = {
@@ -47,6 +57,49 @@ require('nvim-tree').setup({
         edge = "│ ",
         none = "  ",
       },
+    },
+    --default will show icon by default if no icon is provided
+    --default shows no icon by default
+    icons = {
+      webdev_colors = true,
+      git_placement = "before",
+      padding = " ",
+      symlink_arrow = " ➛ ",
+      show = {
+        file = true,
+        folder = true,
+        folder_arrow = true,
+        git = true,
+      },
+      glyphs = {
+        default = '',
+        symlink = '',
+        folder = {
+          arrow_open = "",
+          arrow_closed = "",
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+          symlink_open = "",
+        },
+        git = {
+          unstaged = "✗",
+          staged = "✓",
+          unmerged = "",
+          renamed = "➜",
+          untracked = "★",
+          deleted = "",
+          ignored = "◌"
+        },
+      },
+    },
+    --List of filenames that gets highlighted with NvimTreeSpecialFile
+    special_files = {
+      'README.md',
+      'Makefile',
+      'MAKEFILE'
     },
   },
   hijack_directories = {
