f5880ef07530bd49a943c2c6a19ad0e4d3b30750
Metehan Gulac <metehanglc@protonmail.com>
2022-07-25 14:07:40 +0300

    feat: flake8, black, prettierd.

--- nvim/lua/plugins/null-ls-nvim/settings.lua
+++ nvim/lua/plugins/null-ls-nvim/settings.lua
@@ -2,8 +2,12 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
 	sources = {
+    -- Linter
 		null_ls.builtins.diagnostics.eslint_d,
-		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.diagnostics.flake8,
+    -- Formatter
+		null_ls.builtins.formatting.prettierd,
 		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.black,
 	},
 })
