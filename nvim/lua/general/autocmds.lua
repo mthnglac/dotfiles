727a57d9ec57ff72eeca89b5ba6c73b6fb9d10a1
Metehan Gulac <metehanglc@protonmail.com>
2022-08-09 10:13:07 +0300

    feat: console.log shortcut in js&ts files

--- nvim/lua/general/autocmds.lua
+++ nvim/lua/general/autocmds.lua
@@ -1,9 +1,20 @@
 -- highlight on yank!!!
 vim.api.nvim_create_autocmd("TextYankPost", {
-	pattern = "*",
-	callback = function()
-		require("vim.highlight").on_yank({ timeout = 400 })
-	end,
+  pattern = "*",
+  callback = function()
+    require("vim.highlight").on_yank({ timeout = 400 })
+  end,
+})
+
+-- console.log current word in JS, JSX, TS, TSX
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "javascript, javascriptreact, typescript, typescriptreact",
+  callback = function()
+    Keybind.g({
+      { "n", "<Leader>cl", [["ayiwoconsole.log('<C-R>a:', <C-R>a);<Esc>]], { noremap = true, silent = true } },
+      { "x", "<Leader>cl", [["ayoconsole.log('<C-R>a:', <C-R>a);<Esc>]], { noremap = true, silent = true } },
+    })
+  end,
 })
 
 --local groupCursorEffect = vim.api.nvim_create_augroup("HighlighMyCursor", { clear = true })
