885ae4ac28ae7096bcbbb2536ed64c55146794bb
Metehan Gulac <metehanglc@protonmail.com>
2022-08-22 10:19:41 +0300

    refactor on diffview-nvim

--- nvim/lua/plugins/diffview-nvim/settings.lua
+++ nvim/lua/plugins/diffview-nvim/settings.lua
@@ -1,9 +1,10 @@
 -- Lua
-local cb = require("diffview.config").diffview_callback
+local actions = require("diffview.actions")
 
 require("diffview").setup({
 	diff_binaries = false, -- Show diffs for binaries
 	enhanced_diff_hl = false, -- See ':h diffview-config-enhanced_diff_hl'
+  git_cmd = { "git" },
 	use_icons = true, -- Requires nvim-web-devicons
 	icons = { -- Only applies when use_icons is true.
 		folder_closed = "",
@@ -49,70 +50,74 @@ require("diffview").setup({
 			height = 16,
 		},
 	},
+  commit_log_panel = {
+    win_config = {},
+  },
 	default_args = { -- Default args prepended to the arg-list for the listed commands
 		DiffviewOpen = {},
 		DiffviewFileHistory = {},
 	},
-	key_bindings = {
+  hooks = {},
+	keymaps = {
 		disable_defaults = false, -- Disable the default key bindings
 		-- The `view` bindings are active in the diff buffers, only when the current
 		-- tabpage is a Diffview.
 		view = {
-			["<tab>"] = cb("select_next_entry"), -- Open the diff for the next file
-			["<s-tab>"] = cb("select_prev_entry"), -- Open the diff for the previous file
-			["gf"] = cb("goto_file"), -- Open the file in a new split in previous tabpage
-			["<C-w><C-f>"] = cb("goto_file_split"), -- Open the file in a new split
-			["<C-w>gf"] = cb("goto_file_tab"), -- Open the file in a new tabpage
-			["<leader>e"] = cb("focus_files"), -- Bring focus to the files panel
-			["<leader>b"] = cb("toggle_files"), -- Toggle the files panel.
+      ["<tab>"] = actions.select_next_entry, -- Open the diff for the next file
+      ["<s-tab>"] = actions.select_prev_entry, -- Open the diff for the previous file
+      ["gf"] = actions.goto_file, -- Open the file in a new split in previous tabpage
+      ["<C-w><C-f>"] = actions.goto_file_split, -- Open the file in a new split
+      ["<C-w>gf"] = actions.goto_file_tab, -- Open the file in a new tabpage
+      ["<leader>e"] = actions.focus_files, -- Bring focus to the files panel
+      ["<leader>b"] = actions.toggle_files, -- Toggle the files panel.
 		},
 		file_panel = {
-			["j"] = cb("next_entry"), -- Bring the cursor to the next file entry
-			["<down>"] = cb("next_entry"),
-			["k"] = cb("prev_entry"), -- Bring the cursor to the previous file entry.
-			["<up>"] = cb("prev_entry"),
-			["<cr>"] = cb("select_entry"), -- Open the diff for the selected entry.
-			["o"] = cb("select_entry"),
-			["<2-LeftMouse>"] = cb("select_entry"),
-			["-"] = cb("toggle_stage_entry"), -- Stage / unstage the selected entry.
-			["S"] = cb("stage_all"), -- Stage all entries.
-			["U"] = cb("unstage_all"), -- Unstage all entries.
-			["X"] = cb("restore_entry"), -- Restore entry to the state on the left side.
-			["R"] = cb("refresh_files"), -- Update stats and entries in the file list.
-			["<tab>"] = cb("select_next_entry"),
-			["<s-tab>"] = cb("select_prev_entry"),
-			["gf"] = cb("goto_file"),
-			["<C-w><C-f>"] = cb("goto_file_split"),
-			["<C-w>gf"] = cb("goto_file_tab"),
-			["i"] = cb("listing_style"), -- Toggle between 'list' and 'tree' views
-			["f"] = cb("toggle_flatten_dirs"), -- Flatten empty subdirectories in tree listing style.
-			["<leader>e"] = cb("focus_files"),
-			["<leader>b"] = cb("toggle_files"),
+      ["j"] = actions.next_entry, -- Bring the cursor to the next file entry
+      ["<down>"] = actions.next_entry,
+      ["k"] = actions.prev_entry, -- Bring the cursor to the previous file entry.
+      ["<up>"] = actions.prev_entry,
+      ["<cr>"] = actions.select_entry, -- Open the diff for the selected entry.
+      ["o"] = actions.select_entry,
+      ["<2-LeftMouse>"] = actions.select_entry,
+      ["-"] = actions.toggle_stage_entry, -- Stage / unstage the selected entry.
+      ["S"] = actions.stage_all, -- Stage all entries.
+      ["U"] = actions.unstage_all, -- Unstage all entries.
+      ["X"] = actions.restore_entry, -- Restore entry to the state on the left side.
+      ["R"] = actions.refresh_files, -- Update stats and entries in the file list.
+      ["<tab>"] = actions.select_next_entry,
+      ["<s-tab>"] = actions.select_prev_entry,
+      ["gf"] = actions.goto_file,
+      ["<C-w><C-f>"] = actions.goto_file_split,
+      ["<C-w>gf"] = actions.goto_file_tab,
+      ["i"] = actions.listing_style, -- Toggle between 'list' and 'tree' views
+      ["f"] = actions.toggle_flatten_dirs, -- Flatten empty subdirectories in tree listing style.
+      ["<leader>e"] = actions.focus_files,
+      ["<leader>b"] = actions.toggle_files,
 		},
 		file_history_panel = {
-			["g!"] = cb("options"), -- Open the option panel
-			["<C-A-d>"] = cb("open_in_diffview"), -- Open the entry under the cursor in a diffview
-			["y"] = cb("copy_hash"), -- Copy the commit hash of the entry under the cursor
-			["zR"] = cb("open_all_folds"),
-			["zM"] = cb("close_all_folds"),
-			["j"] = cb("next_entry"),
-			["<down>"] = cb("next_entry"),
-			["k"] = cb("prev_entry"),
-			["<up>"] = cb("prev_entry"),
-			["<cr>"] = cb("select_entry"),
-			["o"] = cb("select_entry"),
-			["<2-LeftMouse>"] = cb("select_entry"),
-			["<tab>"] = cb("select_next_entry"),
-			["<s-tab>"] = cb("select_prev_entry"),
-			["gf"] = cb("goto_file"),
-			["<C-w><C-f>"] = cb("goto_file_split"),
-			["<C-w>gf"] = cb("goto_file_tab"),
-			["<leader>e"] = cb("focus_files"),
-			["<leader>b"] = cb("toggle_files"),
+      ["g!"] = actions.options, -- Open the option panel
+      ["<C-A-d>"] = actions.open_in_diffview, -- Open the entry under the cursor in a diffview
+      ["y"] = actions.copy_hash, -- Copy the commit hash of the entry under the cursor
+      ["zR"] = actions.open_all_folds,
+      ["zM"] = actions.close_all_folds,
+      ["j"] = actions.next_entry,
+      ["<down>"] = actions.next_entry,
+      ["k"] = actions.prev_entry,
+      ["<up>"] = actions.prev_entry,
+      ["<cr>"] = actions.select_entry,
+      ["o"] = actions.select_entry,
+      ["<2-LeftMouse>"] = actions.select_entry,
+      ["<tab>"] = actions.select_next_entry,
+      ["<s-tab>"] = actions.select_prev_entry,
+      ["gf"] = actions.goto_file,
+      ["<C-w><C-f>"] = actions.goto_file_split,
+      ["<C-w>gf"] = actions.goto_file_tab,
+      ["<leader>e"] = actions.focus_files,
+      ["<leader>b"] = actions.toggle_files,
 		},
 		option_panel = {
-			["<tab>"] = cb("select"),
-			["q"] = cb("close"),
+      ["<tab>"] = actions.select_entry,
+      ["q"] = actions.close,
 		},
 	},
 })
