30bb1ebda801795c430f45eee330b446cb16ff1b
Metehan Gulac <metehanglc@protonmail.com>
2022-11-21 23:13:36 +0100

    ref: changed plugin confs

--- nvim/lua/plugins/nvim-cmp/settings.lua
+++ nvim/lua/plugins/nvim-cmp/settings.lua
@@ -1,6 +1,6 @@
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 local lspconfig = require("lspconfig")
 
