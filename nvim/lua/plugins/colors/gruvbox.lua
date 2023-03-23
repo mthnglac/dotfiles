aeb3c77234d18382ac1f31856487a9af2a955477
Metehan <metehanglc@protonmail.com>
2023-03-23 09:30:28 +0100

    refactor: gruvbox confs updated

--- nvim/lua/plugins/colors/gruvbox.lua
+++ nvim/lua/plugins/colors/gruvbox.lua
@@ -2,7 +2,11 @@ require("gruvbox").setup({
 	undercurl = true,
 	underline = true,
 	bold = true,
-	italic = true, -- will make italic comments and special strings
+  italic = {
+    strings = true,
+    operators = true,
+    comments = true,
+  },
 	inverse = true, -- invert background for search, diffs, statuslines and errors
 	invert_selection = false,
 	invert_signs = false,
