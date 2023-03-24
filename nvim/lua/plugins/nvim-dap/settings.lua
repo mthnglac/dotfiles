c322201403915540aaf54b4645dd239b803d7caa
Metehan <metehanglc@protonmail.com>
2023-03-24 20:25:30 +0100

    feat: nvim-dap-ui plugin

--- nvim/lua/plugins/nvim-dap/settings.lua
+++ nvim/lua/plugins/nvim-dap/settings.lua
@@ -1,33 +1,59 @@
 local dap = require("dap")
 
-dap.adapters.chrome = {
+-- dap.adapters.chrome = {
+-- 	type = "executable",
+-- 	command = "node",
+-- 	args = { os.getenv("HOME") .. "/.local/share/nvim/mason/packages/chrome-debug-adapter/out/src/chromeDebug.js" },
+-- }
+
+dap.adapters.node2 = {
 	type = "executable",
 	command = "node",
-	args = { os.getenv("HOME") .. "/.local/share/nvim/mason/packages/chrome-debug-adapter/out/src/chromeDebug.js" },
+	args = { vim.fn.stdpath("data") .. "/mason/packages/node-debug2-adapter/out/src/nodeDebug.js" },
 }
 
-dap.configurations.javascript = { -- change this to javascript if needed
+dap.configurations.javascript = {
 	{
-		type = "chrome",
-		request = "attach",
+		name = "Launch",
+		type = "node2",
+		request = "launch",
 		program = "${file}",
 		cwd = vim.fn.getcwd(),
 		sourceMaps = true,
 		protocol = "inspector",
-		port = 9222,
-		webRoot = "${workspaceFolder}",
+		console = "integratedTerminal",
+	},
+	{
+		-- For this to work you need to make sure the node process is started with the `--inspect` flag.
+		name = "Attach to process",
+		type = "node2",
+		request = "attach",
+		processId = require("dap.utils").pick_process,
 	},
 }
 
-dap.configurations.typescriptreact = { -- change to typescript if needed
-	{
-		type = "chrome",
-		request = "attach",
-		program = "${file}",
-		cwd = vim.fn.getcwd(),
-		sourceMaps = true,
-		protocol = "inspector",
-		port = 9222,
-		webRoot = "${workspaceFolder}",
-	},
-}
+-- dap.configurations.javascript = { -- change this to javascript if needed
+-- 	{
+-- 		type = "chrome",
+-- 		request = "attach",
+-- 		program = "${file}",
+-- 		cwd = vim.fn.getcwd(),
+-- 		sourceMaps = true,
+-- 		protocol = "inspector",
+-- 		port = 9222,
+-- 		webRoot = "${workspaceFolder}",
+-- 	},
+-- }
+
+-- dap.configurations.typescriptreact = { -- change to typescript if needed
+-- 	{
+-- 		type = "chrome",
+-- 		request = "attach",
+-- 		program = "${file}",
+-- 		cwd = vim.fn.getcwd(),
+-- 		sourceMaps = true,
+-- 		protocol = "inspector",
+-- 		port = 9222,
+-- 		webRoot = "${workspaceFolder}",
+-- 	},
+-- }
