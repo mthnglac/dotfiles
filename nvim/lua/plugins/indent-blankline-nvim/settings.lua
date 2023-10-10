daac66e4efeb889cf43b73c636a2e0c105ad60d0
Metehan Gulac <metehanglc@protonmail.com>
2023-10-10 10:51:33 +0200

    ref: indent-blankline-nvim changed conf

--- nvim/lua/plugins/indent-blankline-nvim/settings.lua
+++ nvim/lua/plugins/indent-blankline-nvim/settings.lua
@@ -1,6 +1,14 @@
-require("indent_blankline").setup({
-	char_list = { "│", "¦", "┆", "┊" },
-	buftype_exclude = { "terminal" },
-	show_current_context = "v:true",
-	use_treesitter = "v:true",
+require("ibl").setup({
+	debounce = 100,
+	indent = { char = "|" },
+	whitespace = { highlight = { "Whitespace", "NonText" } },
+	scope = { exclude = { language = { "lua" } } },
 })
+-- require("ibl").setup({
+-- 	char_list = { "│", "¦", "┆", "┊" },
+-- 	buftype_exclude = { "terminal" },
+-- 	show_current_context = "v:true",
+-- 	use_treesitter = "v:true",
+-- })
+
+
