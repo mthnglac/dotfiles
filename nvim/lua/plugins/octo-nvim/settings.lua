3abb2643572cb8302dd7a0d39fc685b2f7351397
Metehan Gulac <metehan@Mac.fritz.box>
2025-08-09 15:43:08 +0200

    conf: octo-nvim changes conf

--- nvim/lua/plugins/octo-nvim/settings.lua
+++ nvim/lua/plugins/octo-nvim/settings.lua
@@ -4,7 +4,7 @@ require("octo").setup({
 	reaction_viewer_hint_icon = "", -- marker for user reactions
 	user_icon = " ", -- user icon
 	timeline_marker = "", -- timeline marker
-	timeline_indent = "2", -- timeline indentation
+	timeline_indent = 2, -- timeline indentation
 	right_bubble_delimiter = "", -- bubble delimiter
 	left_bubble_delimiter = "", -- bubble delimiter
 	github_hostname = "", -- GitHub Enterprise host
