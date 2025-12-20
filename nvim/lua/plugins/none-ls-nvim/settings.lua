3bfb6cc998423e41e9bbf08589468faaab118a2a
Metehan Gulac <metehanglc@protonmail.com>
2025-12-20 02:29:11 +0100

    conf changes

--- nvim/lua/plugins/none-ls-nvim/settings.lua
+++ nvim/lua/plugins/none-ls-nvim/settings.lua
@@ -1,14 +1,15 @@
 local null_ls = require("null-ls")
 
 null_ls.setup({
-  sources = {
-    -- Linter
-    -- ...
+	sources = {
+		-- Linter
+		-- ...
 
-    -- Formatter
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.formatting.rubocop,
-    null_ls.builtins.formatting.sql_formatter,
-  },
+		-- Formatter
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.prettierd,
+		null_ls.builtins.formatting.rubyfmt,
+		null_ls.builtins.formatting.sql_formatter,
+		null_ls.builtins.formatting.erb_format,
+	},
 })
